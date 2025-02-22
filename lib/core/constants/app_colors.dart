37027095b69c0f78a539d158a32f41c522846445
baloch_backery <itsbalouch001@gmail.com>
2025-02-23 00:28:07 +0500

    Folder Structured

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const Color primary = Color(0xFFBBF246);
+  static const Color primaryDark = Color(0xFF192126);
+
+  // Background Colors
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color cardBackground = Color(0xFF384046);
+
+  // Text Colors
+  static const Color textPrimary = Color(0xff192126);
+  static const Color trainingBarColor = Color(0xffA48AED);
+  static const Color textWhite = Color(0xFFFFFFFF);
+  static const Color waterColor = Color(0xff95CCE3);
+  static const Color motivateColor = Color(0xffF6CFCF);
+  static const Color heartRateColor = Color(0xffED4747);
+  static const Color stepBarColor = Color(0xffFFE8C6);
+  static const Color stepBarDarkColor = Color(0xffFCC46F);
+
+
+  
+
+
+
+
+}
---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,23 @@
+class AppImages {
+  // Icons
+  static const String homeIcon = '';
+  static const String profileIcon = '';
+  static const String settingsIcon = '';
+
+  // Backgrounds
+  static const String loginBg = '';
+  static const String dashboardBg = '';
+  static const String profileBg = '';
+
+  // Logos
+  static const String logo = '';
+  static const String darkLogo = '';
+
+  // Products
+  static const String product1 = '';
+  static const String product2 = '';
+
+  // Users
+  static const String user1 = '';
+  static const String user2 = '';
+}
