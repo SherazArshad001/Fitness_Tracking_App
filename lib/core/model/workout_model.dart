12df1903aa9c10d94a2d8b40b3d5a368d1456b33
baloch_backery <itsbalouch001@gmail.com>
2025-02-24 23:30:34 +0500

    Helper & model in core Folder added

---
+++ lib/core/model/workout_model.dart
@@ -0,0 +1,17 @@
+class WorkoutCard {
+  final String backgroundImage;
+  final String title;
+  final String icon1;
+  final String text1;
+  final String icon2;
+  final String text2;
+
+  const WorkoutCard({
+    required this.backgroundImage,
+    required this.title,
+    required this.icon1,
+    required this.text1,
+    required this.icon2,
+    required this.text2,
+  });
+}
